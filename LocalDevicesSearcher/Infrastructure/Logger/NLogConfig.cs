ece2aafd7d4a316ae17cb8795c1e43cbaa5345cb
AmawaH <110606157+AmawaH@users.noreply.github.com>
2023-12-07 16:43:49 +0200

    Changing the NLog configuration from the programmed one to NLog.config.

--- LocalDevicesSearcher/Infrastructure/Logger/NLogConfig.cs
+++ LocalDevicesSearcher/Infrastructure/Logger/NLogConfig.cs
@@ -1,9 +1,6 @@
 ﻿using Microsoft.Extensions.Logging;
 using NLog.Common;
-using NLog.Config;
 using NLog.Extensions.Logging;
-using NLog.Targets;
-using System;
 
 namespace LocalDevicesSearcher.Infrastructure.Logger
 {
@@ -13,25 +10,6 @@ namespace LocalDevicesSearcher.Infrastructure.Logger
         {
             InternalLogger.LogToConsole = true;
             InternalLogger.LogToConsoleError = true;
-            var config = new LoggingConfiguration();
-            string datetime = DateTime.Now.ToString("yyyyMMdd_HHmmss");
-            string fileName = $"{datetime}.log";
-            //Targets:
-            var fileTarget = new FileTarget("fileTarget")
-            {
-                FileName = fileName,
-                Layout = "${longdate}|${level:uppercase=true}|${message} ${exception:format=ToString}"
-            };
-            var consoleTarget = new ConsoleTarget("consoleTarget")
-            {
-                Layout = "${longdate}|${level:uppercase=true}|${message} ${exception:format=ToString}"
-            };
-            config.AddTarget(fileTarget);
-            config.AddTarget(consoleTarget);
-            //Rules:
-            config.AddRule(NLog.LogLevel.Trace, NLog.LogLevel.Fatal, consoleTarget);
-            config.AddRule(NLog.LogLevel.Info, NLog.LogLevel.Fatal, fileTarget);
-            NLog.LogManager.Configuration = config;
         }
         public ILogger GetNLogLogger()
         {
